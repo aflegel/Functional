f3980497fbfb4d807cdb18a02292f896f6bfe02d
Alexander Boutin Flegel <alexander.boutinFlegel@propellerhead.co.nz>
2020-08-18 10:07:25 +1200

    [fluent-extensions] WIP Added OptionExtension * Packageable class library * Has issue with existing Should() Extension

---
+++ Functional.Fluent/OptionAssertions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+
+namespace Functional.Fluent
+{
+	public class OptionsAssertions<T> : ReferenceTypeAssertions<Option<T>, OptionsAssertions<T>>
+	{
+		private T Exception => throw new Exception("Unexpected Result");
+
+		private Exception InvokeSubjectWithInterception(Func<T, T> some, Func<T> none)
+		{
+			try
+			{
+				Subject.Match(some, none);
+			}
+			catch (Exception exc)
+			{
+				return exc;
+			}
+			return null;
+		}
+
+		public OptionsAssertions(Option<T> instance) => Subject = instance;
+
+		protected override string Identifier => "option";
+
+		public AndConstraint<OptionsAssertions<T>> BeSome(string because = "", params object[] becauseArgs)
+		{
+			Execute.Assertion
+				.BecauseOf(because, becauseArgs)
+				.Given(() => InvokeSubjectWithInterception(s => s, () => Exception))
+				.ForCondition(s => s == null)
+				.FailWith("Expected {context:option} to be Some but found None.");
+			return new AndConstraint<OptionsAssertions<T>>(this);
+		}
+
+		public AndConstraint<OptionsAssertions<T>> BeSome(T value, string because = "", params object[] becauseArgs)
+		{
+			Execute.Assertion
+				.BecauseOf(because, becauseArgs)
+				.Given(() => InvokeSubjectWithInterception(s => s, () => Exception))
+				.ForCondition(s => s == null)
+				.FailWith("Expected {context:option} to be {0} but found None.", _ => value);
+
+			Execute.Assertion
+				.BecauseOf(because, becauseArgs)
+				.Given(() => Subject.Match(s => s, () => Exception))
+				.ForCondition(s => s.Equals(value))
+				.FailWith("Expected {context:option} to be {0} but found {1}.", _ => value, s => s);
+
+			return new AndConstraint<OptionsAssertions<T>>(this);
+		}
+
+		public AndConstraint<OptionsAssertions<T>> BeNone(string because = "", params object[] becauseArgs)
+		{
+			Execute.Assertion
+				.BecauseOf(because, becauseArgs)
+				.Given(() => InvokeSubjectWithInterception(s => s, () => Exception))
+				.ForCondition(s => s != null)
+				.FailWith("Expected {context:option} to be None but found Some.");
+			return new AndConstraint<OptionsAssertions<T>>(this);
+		}
+	}
+}
---
+++ Functional.Fluent/OptionAssertionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Functional.Fluent
+{
+	public static class FluentOptionExtensions
+	{
+		public static OptionsAssertions<TSome> Should<TSome>(this Option<TSome> instance)
+			=> new OptionsAssertions<TSome>(instance);
+
+		public static async Task<OptionsAssertions<TSome>> Should<TSome>(this Task<Option<TSome>> instance)
+			=> new OptionsAssertions<TSome>(await instance);
+
+		public static async Task<AndConstraint<OptionsAssertions<TSome>>> BeSomeAsync<TSome>(this Task<OptionsAssertions<TSome>> test, string because = "", params object[] becauseArgs)
+			=> (await test).BeSome(because, becauseArgs);
+
+		public static async Task<AndConstraint<OptionsAssertions<TSome>>> BeSomeAsync<TSome>(this Task<OptionsAssertions<TSome>> test, TSome value, string because = "", params object[] becauseArgs)
+			=> (await test).BeSome(value, because, becauseArgs);
+
+		public static async Task<AndConstraint<OptionsAssertions<TSome>>> BeNoneAsync<TSome>(this Task<OptionsAssertions<TSome>> test, string because = "", params object[] becauseArgs)
+			=> (await test).BeNone(because, becauseArgs);
+	}
+}
